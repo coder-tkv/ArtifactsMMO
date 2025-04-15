5b6e55dd6f9beafdeb890a80cafd29d9b10be95d
tkv <egahaker@mail.ru>
2025-04-15 21:37:49 +0300

    added unequip

---
+++ player_unequip.py
@@ -0,0 +1,20 @@
+import requests
+import os
+API_TOKEN = os.getenv('API_TOKEN')
+SERVER = "https://api.artifactsmmo.com"
+CHARACTER = 'tkv'
+
+url = SERVER + '/my/' + CHARACTER + '/action/unequip'
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    'Authorization': f'Bearer {API_TOKEN}'
+}
+
+data = {
+    "slot": "weapon"
+}
+
+response = requests.post(url, headers=headers, json=data, allow_redirects=True)
+print(response.json())
