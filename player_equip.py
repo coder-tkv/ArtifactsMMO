7a63ed39d1d6e3e152a5b267275cf69f45d2ded0
tkv <egahaker@mail.ru>
2025-04-15 21:41:27 +0300

    added equip

---
+++ player_equip.py
@@ -0,0 +1,21 @@
+import requests
+import os
+API_TOKEN = os.getenv('API_TOKEN')
+SERVER = "https://api.artifactsmmo.com"
+CHARACTER = 'tkv'
+
+url = SERVER + '/my/' + CHARACTER + '/action/equip'
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    'Authorization': f'Bearer {API_TOKEN}'
+}
+
+data = {
+    "code": "wooden_staff",
+    "slot": "weapon"
+}
+
+response = requests.post(url, headers=headers, json=data, allow_redirects=True)
+print(response.json())
